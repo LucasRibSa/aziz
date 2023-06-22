3a4de6c2db36fdf3bb879fd30b3aa0f128d7aadd
Lucas Ribeiro <lucasribsa@icloud.com>
2023-06-22 07:43:13 -0300

    adicionando a pagina de jalecos

--- AZIZ/script.js
+++ AZIZ/script.js
@@ -7,24 +7,8 @@ setInterval( function(){
 
 function nextImage() {
     count++;
-    if(count>7){
+    if(count>32){
         count =1;
     }
-
     document.getElementById("radio"+count).checked = true;
-}
-
-let count2 = 8;
-document.getElementById("radio8").checked = true;
-
-setInterval(function(){
-    nextImage2();
-}, 10000)
-
-function nextImage2() {
-    count2++;
-    if(count2>21){
-        count2 = 8;
-    }
-    document.getElementById("radio"+count2).checked = true;
 }
